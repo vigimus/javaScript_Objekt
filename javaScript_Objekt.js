485e088e0bc4260634b83df4d24d3695d4637598
vigimus <160164175+vigimus@users.noreply.github.com>
2024-04-16 18:56:19 +0200

    Add files via upload

---
+++ javaScript_Objekt.js
@@ -0,0 +1,28 @@
+var user = {
+	namn: "Viktor",
+	alder: 37,
+	hobby: "Baseboll" , 
+	arGift: false, 
+};
+
+//För att ta fram property värdena kan man t.ex skriva
+// user.namn eller user.hobby e.t.c
+
+//user.favoritMat = "Börgare"; kommer lägga till det i ojektet "user"
+
+//user.arGift = true; så kommer vi ändra svaret för arGift värdet i ojektet "user"
+
+var list = [
+{
+anvandarNamn = "Fredrik",
+losenord = "asdasdweee",	
+}, 
+{
+anvandarNamn: "Anna",
+losenord: "12312sdfs"
+}
+
+];
+
+//Det finns något som kallas för null det betyder att det inte
+//Finns något där
